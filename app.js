90e5ae5d138c45d8d280d3ada5ec6cdeccdaf81e
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-02-27 13:36:45 +0600

    dog list added

--- app.js
+++ app.js
@@ -12,10 +12,16 @@ const displayDog = (doglist) => {
     const main = document.getElementById("main")
 
     const firstTenData = doglist.slice(0, 10)
-    const div = document.createElement("div")
+
     for (const dog of firstTenData) {
-        console.log(dog.name)
-        div.innerHTML = `<h2>${dog.name} </h2>`
+
+        console.log(firstTenData)
+        const div = document.createElement("div")
+        console.log(dog.image.url)
+        div.className = "col-lg-4"
+        div.innerHTML = `<h2>${dog.name}</h2> <p>${dog.weight.imperial}</p>   <p>${dog.temperament} </p>    <img width="400px" heigt="250px"  src="${dog.image.url}" /> `
+
+        main.appendChild(div)
     }
 
 
