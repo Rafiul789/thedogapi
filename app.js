87c7936b45e183ff246dea5e15087435c9f86cd1
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-02-27 11:48:17 +0600

    api data added

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const loadDog = () => {
     fetch('https://api.thedogapi.com/v1/breeds')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayDog(data))
+
+
+}
+
+const displayDog = (doglist) => {
+
+
+    const main = document.getElementById("main")
+
+    const firstTenData = doglist.slice(0, 10)
+    const div = document.createElement("div")
+    for (const dog of firstTenData) {
+        console.log(dog.name)
+        div.innerHTML = `<h2>${dog.name} </h2>`
+    }
 
 
 }
